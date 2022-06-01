c54700b2a0ad1988336843da0870cd4007af0375
luck2515 <luck2515dev@gmail.com>
2022-06-01 16:13:48 +0900

    first commit

---
+++ src/components/templates/index.tsx
@@ -0,0 +1,16 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p>Learn Solid</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
