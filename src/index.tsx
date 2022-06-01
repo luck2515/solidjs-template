5462a096fbb53e9c33273f6e7c415ca3f8eba92d
luck2515 <luck2515dev@gmail.com>
2022-06-01 16:13:48 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "solid-js/web";
+
+import "./styles/index.css";
+import App from "./pages/App";
+
+render(() => <App />, document.getElementById("root") as HTMLElement);
---
+++ src/pages/App.tsx
@@ -0,0 +1,9 @@
+import type { Component } from "solid-js";
+
+import Top from "../components/templates/index";
+
+const App: Component = () => {
+  return <Top />;
+};
+
+export default App;
